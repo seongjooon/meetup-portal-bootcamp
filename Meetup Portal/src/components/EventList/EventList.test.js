d3c8a44eccc2c3b9a7a06fca436c9714fec0731d
MrKamake <seongjooon@gmail.com>
2019-08-28 13:57:39 +0900

    create test code(Bookmark, EventList)

--- Meetup Portal/src/components/EventList/EventList.test.js
+++ Meetup Portal/src/components/EventList/EventList.test.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Bookmark from './Bookmark';
+import EventList from './EventList';
 
-describe('<Bookmark /> Component', () => {
-  let bookmarkList = [];
+describe('<EventList /> Component', () => {
+  let eventList = [];
 
   beforeEach(() => {
-    bookmarkList = [
+    eventList = [
       {
         id: '019238',
-        name: 'kamake',
+        name: 'hayake',
         local_date: '2019-11-03',
         local_time: '09:00',
         yes_rsvp_count: 3,
@@ -18,20 +18,20 @@ describe('<Bookmark /> Component', () => {
     ];
   });
 
-  it('should be rendered <div class="bookmark-element"> as long as the length of the bookmarklist which is props.', () => {
-    let wrapper = shallow(<Bookmark bookmarkList={bookmarkList} />);
+  it('should be rendered <div class="bookmark-element"> as long as the length of the eventlist which is props.', () => {
+    let wrapper = shallow(<EventList eventList={eventList} />);
     expect(wrapper.find('.bookmark-element').length).toBe(1);
 
-    bookmarkList.push({
+    eventList.push({
       id: '102938',
-      name: 'hayake',
+      name: 'kamake',
       local_date: '2019-12-11',
       local_time: '09:00',
       yes_rsvp_count: 3,
       group: { name: 'alskdj' }
     });
 
-    wrapper = shallow(<bookmarkList bookmarkList={bookmarkList} />);
+    wrapper = shallow(<EventList eventList={eventList} />);
     expect(wrapper.find('.bookmark-element').length).toBe(2);
   });
 });
